89a6b28c4c3e3f7b1a5199aea4f32c9998b1cb1e
upload <140815317+myliu2048@users.noreply.github.com>
2023-11-18 19:34:29 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -6,11 +6,11 @@
 <!-- This repo contains the code and data of our CVPR'2024 paper [Progressive Contrastive Learning for Fusion-style Deep Multi-view Clustering](https://openaccess.thecvf.com/content/CVPR2023/papers/Yan_GCFAgg_Global_and_Cross-View_Feature_Aggregation_for_Multi-View_Clustering_CVPR_2023_paper.pdf). -->
 
 
-<!-- ## 1. Framework
+<!-- ###  Framework
 
-<img src="fig/framework.png"  width="897" height="317" />
+<img src="framework.png"  width="897" height="317" />
 
- The framework of PCL-FDMC. We first employ the autoencoders with MSE reconstruction loss to learn low-level view-specific embeddings $\mathbf{Z}^v$ from raw multi-view data. We then concatenate the embeddings from individual views and perform locally consistent aggregation on concatenated representation $\mathbf{Z}$ to obtain the fusion representation $\hat{\mathbf{Z}}$. Low-level embeddings $\mathbf{Z}^v$ and the fusion representation $\hat{\mathbf{Z}}$ are further mapped into the high-level  space to get $\mathbf{H}^v$ and enhanced fusion representation $\hat{\mathbf{H}}$. MSE denotes Mean Square Error loss; TEL denotes Transformer Encoder Layer; MLP denotes Multi-Layer Perception; NMCL denotes Neighbor-Masked Contrastive Loss. -->
+ <!-- The framework of PCL-FDMC. We first employ the autoencoders with MSE reconstruction loss to learn low-level view-specific embeddings $\mathbf{Z}^v$ from raw multi-view data. We then concatenate the embeddings from individual views and perform locally consistent aggregation on concatenated representation $\mathbf{Z}$ to obtain the fusion representation $\hat{\mathbf{Z}}$. Low-level embeddings $\mathbf{Z}^v$ and the fusion representation $\hat{\mathbf{Z}}$ are further mapped into the high-level  space to get $\mathbf{H}^v$ and enhanced fusion representation $\hat{\mathbf{H}}$. MSE denotes Mean Square Error loss; TEL denotes Transformer Encoder Layer; MLP denotes Multi-Layer Perception; NMCL denotes Neighbor-Masked Contrastive Loss. --> 
 
 ### Requirements
 
@@ -20,24 +20,22 @@ pytorch==1.7.1+cu110
 
 numpy==1.21.2
 
-scikit-learn>=0.23.2
+scikit-learn==0.23.2
 
 ### Datasets
 
-All datasets are placed in "data" folder. 
+MNIST_USPS and Hdigit are placed in "data" folder, Prokaryotic and Caltech can be downloaded from [dropbox](https://www.dropbox.com/scl/fi/shv57hvaq1xo7teurt6xh/data.zip?rlkey=lo05a9iegemt9tuhpe5osj1du&dl=0), unzip all the data, put in 'data/' folder.
 
 ### Usage
 
-The code includes:
 
-- train a new model：
+- To train a new model, run:
 
 ```bash
 python main.py
 ```
 
-- test the trained model:
-download the trained models from [dropbox](https://www.dropbox.com/scl/fi/dn69vhun6rnfu9xieto2e/models.zip?rlkey=t7lxliqobh145i43n4jpnkd5k&dl=0), and unzip to models, 
+- To test the trained model, download the trained models from [dropbox](https://www.dropbox.com/scl/fi/dn69vhun6rnfu9xieto2e/models.zip?rlkey=t7lxliqobh145i43n4jpnkd5k&dl=0), and unzip all the model named by corresponding data, put in 'models/' folder, and run: 
 ```bash
 python test.py
 ```
---
+++ network.py
@@ -0,0 +1,94 @@
+from torch import nn
+import torch.nn.functional as F
+import torch
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, feature_dim):
+        super(Encoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, 1024), 
+            nn.BatchNorm1d(1024),
+            nn.ReLU(True), 
+            nn.Dropout(0.2),
+            nn.Linear(1024, 1024), 
+            nn.BatchNorm1d(1024),
+            nn.ReLU(True), 
+            nn.Dropout(0.2),
+            nn.Linear(1024, 1024), 
+            nn.BatchNorm1d(1024),
+            nn.ReLU(True), 
+            nn.Dropout(0.2),
+            nn.Linear(1024, feature_dim), nn.BatchNorm1d(feature_dim),
+            nn.ReLU(True)
+        )
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, input_dim, feature_dim):
+        super(Decoder, self).__init__()
+        self.decoder = nn.Sequential(
+            nn.Linear(feature_dim, 1024), 
+            nn.ReLU(), 
+            nn.Dropout(0.2),
+            nn.Linear(1024, 1024), 
+            nn.ReLU(), 
+            nn.Dropout(0.2), 
+            nn.Linear(1024, 1024), 
+            nn.ReLU(), 
+            nn.Dropout(0.2),
+            nn.Linear(1024, input_dim)
+        )
+
+    def forward(self, x):
+        return self.decoder(x)
+
+
+class Model(nn.Module):
+    def __init__(self, args, device):
+        view, input_size, low_feature_dim, high_feature_dim = \
+            args.view, args.dims, args.low_feature_dim, args.high_feature_dim
+        super(Model, self).__init__()
+        self.encoders = []
+        self.decoders = []
+        self.Specific_view = []
+        for v in range(view):
+            self.encoders.append(Encoder(input_size[v], low_feature_dim).to(device))
+            self.decoders.append(Decoder(input_size[v], low_feature_dim).to(device))
+            self.Specific_view.append(nn.Linear(low_feature_dim, high_feature_dim).to(device))
+        self.encoders = nn.ModuleList(self.encoders)
+        self.decoders = nn.ModuleList(self.decoders)
+        self.view = view
+        self.TEL = nn.TransformerEncoderLayer(d_model=low_feature_dim * view, nhead=args.nhead,
+                                                                  dim_feedforward=256)
+        self.Common_view = nn.Sequential(
+            nn.Linear(low_feature_dim * view, high_feature_dim)
+        )
+
+        self.pseudo_clustering_layer = nn.Sequential(
+            nn.Linear(high_feature_dim, args.class_num),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, xs):
+        xrs, zs, hs = [], [], []
+        for v in range(self.view):
+            x = xs[v]
+            z = self.encoders[v](x)
+            h = F.normalize(self.Specific_view[v](z), dim=1)
+            xr = self.decoders[v](z)
+            zs.append(z)
+            xrs.append(xr)
+            hs.append(h)     
+        catZ = torch.cat(zs, 1)
+        catZ = F.normalize(catZ, dim=1)
+        fusedZ = catZ.unsqueeze(1)  
+        fusedZ, S = self.TEL(fusedZ)  
+        fusedZ = fusedZ.squeeze(1)  
+        S = S.squeeze(0)  
+        Hhat = F.normalize(self.Common_view(fusedZ), dim=1)
+        p = self.pseudo_clustering_layer(Hhat)
+        return Hhat, S, xrs, zs, hs, catZ, fusedZ, p
